435da3258bab8964ff968c7414cd1963b9ca24db
Nyomika Jain <nyomikajain12345@gmail.com>
2026-06-01 15:44:33 +0530

    Week 1 Day 1 Leetcode Solutions

---
+++ Week_1/Day_1/Best_Time_To_Buy_And_Sell_Stock.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int buy= INT_MAX;
+        int profit=0;
+        for(int i:prices){
+            buy= min(buy,i);
+            profit= max(profit, i-buy);
+        }
+        return profit;
+    }
+};
