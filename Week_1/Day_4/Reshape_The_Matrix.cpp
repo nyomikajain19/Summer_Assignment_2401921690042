de5091e97f588e9103268edd8efe3b0d04ebe9f7
Nyomika Jain <nyomikajain12345@gmail.com>
2026-06-04 10:49:44 +0530

    Week 1 Day 4 Leetcode Solutions

---
+++ Week_1/Day_4/Reshape_The_Matrix.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<vector<int>> matrixReshape(vector<vector<int>>& mat, int r, int c){
+        int m= mat.size();
+        int n= mat[0].size();
+        if(m*n != r*c){
+            return mat;
+        }
+        vector<int> nums;
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+               nums.push_back(mat[i][j]);
+            }
+        }
+        vector<vector<int>> ans(r, vector<int>(c));
+        int k = 0;
+        for(int i = 0; i < r; i++) {
+            for(int j = 0; j < c; j++) {
+               ans[i][j] = nums[k++];
+            }
+        }
+      return ans;      
+    }
+};
