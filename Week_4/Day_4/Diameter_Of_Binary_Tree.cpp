3b7ad822c32cd251c1dbe1847d52ebb553cb590a
Nyomika Jain <nyomikajain12345@gmail.com>
2026-06-25 11:05:48 +0530

    Week 4 Day 4 Leetcode Solutions

---
+++ Week_4/Day_4/Diameter_Of_Binary_Tree.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int diameter = 0;
+    int depth(TreeNode* root) {
+        if (root == nullptr)
+            return 0;
+        int left = depth(root->left);
+        int right = depth(root->right);
+        diameter = max(diameter, left + right);
+        return 1 + max(left, right);
+    }
+    int diameterOfBinaryTree(TreeNode* root) {
+        depth(root);
+        return diameter;
+    }
+};
